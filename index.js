efb8d563ceed9064b6441add6d9bdfeb565a462e
kevin breid <kevinbreid@yahoo.com>
2022-05-22 13:32:09 -0700

    commit tree 9db7b4d5c2396a8574be6a96fcb8f5489af83f7e

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+    return a + b;
+
+}
+
+function subtract(a, b) {
+return a - b;
+
+}
+
+
+
+function multiply(a, b) {
+    return a * b;
+
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n += 1;
+}
+
+increment(10)
+
+function decrement(n) {
+    return n -= 1;
+}
+
+decrement(10)
+
+function makeInt(string) {
+    parseInt(String, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
